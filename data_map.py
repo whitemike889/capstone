069752dfdf64547003260bed7afab9981bcf0119
Andrew Silva <asilva@law.harvard.edu>
2017-03-27 18:16:31 -0400

    Made alternate version of ALTO mapper which returns the results in a flatter ordered list with each word/space entry directly listed under TAGREFS id listed in the CASEMETS file. I think this will be much easier to use to associate the CASEMETS words with the ALTO words.

--- data_map.py
+++ data_map.py
@@ -74,12 +74,125 @@ def _get_alto_files(case_soup, volume_path):
 
 
 def _map_alto_file(alto_path):
-	#TODO get the map words/lines/blocks/elements of the alto files 
+	'''
+		This will take an alto file, and send back a dictionary like this:
+        'b47-6': {'BL_47.6': {'TL_47.6.1': {'SP_47.6.1.10': {'HPOS': '958', 'VPOS': '2314', 'WIDTH': '16', 'type': 'space'},
+                 'ST_47.6.1.1': {'CC': '00000', 'CONTENT': 'There', 'HEIGHT': '33', 'HPOS': '342', 'STYLEREFS': 'Style_3', 'TAGREFS': '', 'VPOS': '2311', 'WC': '0.58', 'WIDTH': '109', 'type': 'string'},
+		the first element is the element referenced in the CASEMETS file so it should be easy to narrow down to the text block
+
+	'''
 	alto_soup = BeautifulSoup( open(alto_path), "xml")
 
+	alto_contents = {}
+	print(alto_path)
+
+	# First loop through the text blocks
 	for text_block in alto_soup.find("PrintSpace"):
-		print(text_block.name)
-	return []
+		if text_block.name is None:
+			continue
+
+		if text_block['TAGREFS'] not in alto_contents:
+			 alto_contents[text_block['TAGREFS']] = []
+
+
+		#then loop through the text lines
+		for text_line in text_block:
+			if text_line.name is None:
+				continue
+
+			#then loop through strings and spaces
+			for string in text_line:
+				if string.name is None:
+					continue
+				push_dict = {}
+				# at this point, I'm just taking everything in SP and String tags. Not sure if I'll need it all
+				# at the very least, we're going to need the letter location/geometry to split mistakenly 
+				# concatenated strings. I'm sure there's a more efficient way to copy all of these properties
+				# into the map, but this works well enough for now
+				push_dict['text_block'] = text_block['ID']
+				push_dict['text_line'] = text_line['ID']
+				push_dict['id'] = string['ID']
+				push_dict['HPOS'] = string['HPOS']
+				push_dict['VPOS'] = string['VPOS']
+				push_dict['WIDTH'] = string['WIDTH']
+
+				if string.name == 'String':
+					push_dict['type'] = 'string'
+					push_dict['CONTENT'] = string['CONTENT']
+					push_dict['HEIGHT'] = string['HEIGHT']
+					push_dict['STYLEREFS'] = string['STYLEREFS']
+					push_dict['WC'] = string['WC']
+					push_dict['CC'] = string['CC']
+					push_dict['TAGREFS'] = string['TAGREFS']
+				elif string.name == 'SP':
+					push_dict['type'] = 'space'
+				else:
+					raise("encountered a weird string/space tag {} in {}".format(string.name, alto_path))
+				alto_contents[text_block['TAGREFS']].append(push_dict)		
+
+
+	return alto_contents
+
+
+def _alternate_map_alto_file(alto_path):
+	'''
+		This will take an alto file, and send back a dictionary like this:
+        'b47-6': {'BL_47.6': {'TL_47.6.1': {'SP_47.6.1.10': {'HPOS': '958', 'VPOS': '2314', 'WIDTH': '16', 'type': 'space'},
+                 'ST_47.6.1.1': {'CC': '00000', 'CONTENT': 'There', 'HEIGHT': '33', 'HPOS': '342', 'STYLEREFS': 'Style_3', 'TAGREFS': '', 'VPOS': '2311', 'WC': '0.58', 'WIDTH': '109', 'type': 'string'},
+		the first element is the element referenced in the CASEMETS file so it should be easy to narrow down to the text block
+
+	'''
+	alto_soup = BeautifulSoup( open(alto_path), "xml")
+
+	alto_contents = {}
+	print(alto_path)
+
+	# First loop through the text blocks
+	for text_block in alto_soup.find("PrintSpace"):
+		if text_block.name is None:
+			continue
+
+		if text_block['TAGREFS'] not in alto_contents:
+			 alto_contents[text_block['TAGREFS']] = {}
+
+		alto_contents[text_block['TAGREFS']][text_block['ID']] = {}
+
+		#then loop through the text lines
+		for text_line in text_block:
+			if text_line.name is None:
+				continue
+			alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']] = {}
+			#then loop through strings and spaces
+			for string in text_line:
+				if string.name is None:
+					continue
+				alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']] = {}
+
+				# at this point, I'm just taking everything in SP and String tags. Not sure if I'll need it all
+				# at the very least, we're going to need the letter location/geometry to split mistakenly 
+				# concatenated strings. I'm sure there's a more efficient way to copy all of these properties
+				# into the map, but this works well enough for now
+				if string.name == 'SP':
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['type'] = 'space'
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['HPOS'] = string['HPOS']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['VPOS'] = string['VPOS']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['WIDTH'] = string['WIDTH']
+				elif string.name == 'String':
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['type'] = 'string'
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['HPOS'] = string['HPOS']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['VPOS'] = string['VPOS']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['WIDTH'] = string['WIDTH']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['CONTENT'] = string['CONTENT']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['HEIGHT'] = string['HEIGHT']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['STYLEREFS'] = string['STYLEREFS']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['WC'] = string['WC']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['CC'] = string['CC']
+					alto_contents[text_block['TAGREFS']][text_block['ID']][text_line['ID']][string['ID']]['TAGREFS'] = string['TAGREFS']
+
+
+	pprint(alto_contents)
+	return alto_contents
+
 
 def _get_case_string_fs():
 
